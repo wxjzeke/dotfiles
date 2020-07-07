d94806dfef8b7d21de9a803f0971d008194b8158
wxjzeke <563049233@qq.com>
2020-07-07 16:03:23 +0800

    update vscode config

--- vscode/init.sh
+++ vscode/init.sh
@@ -5,3 +5,12 @@ code --install-extension vscodeemacs.emacs
 code --install-extension golang.go
 code --install-extension ms-python.python
 code --install-extension octref.vetur
+code --install-extension rebornix.ruby
+code --install-extension wingrunr21.vscode-ruby
+code --install-extension karunamurti.haml
+code --install-extension gencer.html-slim-scss-css-class-completion
+code --install-extension caixw.statistic # 代码行数统计
+
+code --install-extension alefragnani.project-manager
+
+ln -s ~/Project/src/github.com/wxjzeke/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
